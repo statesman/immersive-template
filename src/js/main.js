5bda016cd18e79aca6cd842fc1a4b192ef7c2b07
Andrew Chavez <adchavez@gmail.com>
2014-12-15 15:09:58 -0600

    Drop hero image markup, JS

--- src/js/main.js
+++ src/js/main.js
@@ -4,48 +4,6 @@
 
   $(function() {
 
-    var introIn = TweenMax.fromTo("#intro", 2,
-      {top: '40%'},
-      {top: '50%'}
-    );
-
-    var heroOut = TweenMax.to("#hero", 1,
-  		{opacity: 0}
-  	);
-
-    var introOut = TweenMax.to("#intro", 1.5,
-      {top: -150, opacity: 0}
-    );
-
-    var storyIn = TweenMax.fromTo("#main", 1.5,
-      {opacity: 0},
-      {opacity: 1}
-    );
-
-    var controller = new ScrollMagic();
-
-    /* Page load animations */
-    introIn.play();
-
-    /* Animations for when a user scrolls past the hero */
-    var scene = new ScrollScene({
-        triggerElement: "#hero-fadeout"
-      })
-      .setTween(introOut)
-      .addTo(controller);
-
-    var scene2 = new ScrollScene({
-        triggerElement: "#hero-fadeout"
-      })
-      .setTween(heroOut)
-      .addTo(controller);
-
-    var scene3 = new ScrollScene({
-        triggerElement: "#hero-fadeout"
-      })
-      .setTween(storyIn)
-      .addTo(controller);
-
     // Open Twitter links in a new window
     $('.sider-twitter').on('click', 'a', function(e) {
       e.preventDefault();
